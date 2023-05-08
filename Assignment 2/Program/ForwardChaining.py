ef35b5b8076d505f87fc55a715cbeb76d4820fad
Hong Quan Ngo <94714009+HongQuanNgo@users.noreply.github.com>
2023-05-09 01:06:17 +1000

    Update ForwardChaining.py

---
+++ Assignment 2/Program/ForwardChaining.py
@@ -0,0 +1,53 @@
+from sentence_transformers import *
+
+class ForwardChaining:
+    def __init__(self, KB, query):
+        self.KB = KB
+        self.query = query
+
+    def __str__(self) -> str:
+        pass
+
+    def fc_entails(self):
+        chain = [] # initialize an array to keep track of the result of forward chaining
+        # initialize the count dictionary
+        """ 
+        Count is a dictionary that keeps count of the number of premises of each clause
+        """
+        count = {}
+        for conjunct in self.KB.conjuncts():
+            if isinstance(conjunct, Implication):
+                count[conjunct] = len(self.KB.conjunct_premise(conjunct))
+        agenda = [str(symbol) for symbol in self.KB.conjuncts() if isinstance(symbol, Symbol) == True]
+        inferred = {symbol : False for symbol in self.KB.symbols()}
+        while agenda:
+            p = agenda.pop(0)
+            chain.append(p)
+            if p == self.query:
+                print("true")
+                chain.append(self.query)
+                return True, chain
+            if inferred[p] == False:
+                inferred[p] = True
+                for conjunct in self.KB.conjuncts():
+                    if isinstance(conjunct, Implication):
+                        if p in self.KB.conjunct_premise(conjunct):
+                            count[conjunct] -= 1
+                            if count[conjunct] == 0:
+                                agenda.append(self.KB.conjunct_conclusion(conjunct))
+        print("False")
+        return False, []
+    
+    def solve(self):
+        solution_found, chain = self.fc_entails()
+        if solution_found:
+            solution = "YES: "
+            for ele in chain:
+                if ele is chain[-1]:
+                    solution += ele
+                else:
+                    solution += ele + ", "
+        else:
+            solution = "NO"
+        return solution
+           
